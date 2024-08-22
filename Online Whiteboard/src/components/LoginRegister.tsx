0e69c972e6f3349d1c28db7030bf740b4c639b89
yonah-trachtman <yonah.trachtman@gmail.com>
2024-08-22 12:37:00 +0300

    Update LoginRegister.tsx

--- Online Whiteboard/src/components/LoginRegister.tsx
+++ Online Whiteboard/src/components/LoginRegister.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+// import axios from "axios";
 import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Box, TextField, Button } from "@mui/material";
